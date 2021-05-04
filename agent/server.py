1f58598f23029a8b1b97ad339bc8808304fd98d5
jetnew <notesjet@gmail.com>
2021-05-04 21:21:30 +0800

    Debugging game server deployment

--- agent/server.py
+++ agent/server.py
@@ -22,6 +22,7 @@ def get_action(state: WorldModel):
 
 if __name__ == "__main__":
     port = int(sys.argv[1])
-    response = requests.post("http://127.0.0.1:8000/connect/", params=(('agent_api', str(port)),))
+    response = requests.post("https://open-worlds.herokuapp.com/connect/", params=(('agent_api', str(port)),))
+    # response = requests.post("http://127.0.0.1:8000/connect/", params=(('agent_api', str(port)),))
     print(f"Connection {json.loads(response.text)['result']}.")
     uvicorn.run(app, host="127.0.0.1", port=port)
