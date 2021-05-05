0d476199b3a8bd3c1d7bbd9eb9e8eb33bd4f84e1
jetnew <notesjet@gmail.com>
2021-05-06 07:51:58 +0800

    Replace matplotlib display with opencv image

--- README.md
+++ README.md
@@ -1,38 +1,7 @@
-# Open Worlds [WIP]
+# Open Worlds
 
 Open Worlds is a MMO grid world game where players deploy agents to survive in an online world.
 
 #### Play Now: [Open Worlds](https://share.streamlit.io/jetnew/open-worlds)
 
 ![image](assets/demo.gif)
-
-## Why?
-A game where players deploy [bots](https://en.wikipedia.org/wiki/Internet_bot) to play a game is not new (e.g. [screeps.com](https://screeps.com)), but because existing games host player-written scripts for the players, it is not scalable to the usage of [artificial intelligence](https://en.wikipedia.org/wiki/Deep_learning) techniques.
-By allowing players to host their own scripts, gameplay can scale to complex emergent agent behaviors.
-
-## Quickstart
-```
-git clone --recurse-submodules https://github.com/jetnew/open-worlds.git
-pip install requirements.txt
-uvicorn game.server:app --port 8000
-streamlit run display/streamlit_app.py
-python agent/server.py 8002
-python agent/server.py 8003
-```
-By default, all services are hosted on localhost (127.0.0.1), the game server runs on port 8000, display server on port 8001, so agent servers should deploy on port 8002+.
-
-## Server Deployment
-Set web concurrency to 1 ([source](https://stackoverflow.com/questions/44292627/python-app-on-heroku-platform-seems-to-start-on-two-threads)):
-```
-heroku config:set -a <game server> WEB_CONCURRENCY=1
-```
-Use 2 procfiles for game and agent servers ([source](https://elements.heroku.com/buildpacks/heroku/heroku-buildpack-multi-procfile)):
-```
-heroku buildpacks:add -a <game server> heroku-community/multi-procfile
-heroku buildpacks:add -a <agent server> heroku-community/multi-procfile
-heroku config:set -a <game server> PROCFILE=Procfile
-heroku config:set -a <agent server> PROCFILE=agent/Procfile
-```
-
-# Development
-* To update submodule [open-worlds-agents](https://github.com/jetnew/open-worlds-agents), run `git fetch` and `git merge origin/master` ([source](https://git-scm.com/book/en/v2/Git-Tools-Submodules)).
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,38 +6,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import psutil
+import pandas as pd
+import cv2
+import PIL
+
 get_ram = lambda: psutil.Process(os.getpid()).memory_info().rss // 1e6
 
+
+# Template
 st.title("Open Worlds")
 st.text("A MMO grid world game where players deploy agents to survive in an online world.")
 st_time = st.empty()
 st_scores = st.empty()
+st.markdown("[Deploy an agent!](https://github.com/jetnew/open-worlds-agents)")
 st_state = st.empty()
-st_ram = st.empty()
-st.markdown("GitHub: [https://github.com/jetnew/open-worlds](https://github.com/jetnew/open-worlds)")
+st.markdown("A game where players deploy [bots](https://en.wikipedia.org/wiki/Internet_bot) to play a game is not new (e.g. [screeps.com](https://screeps.com)), but because existing games host player-written scripts for the players, it is not scalable to the usage of [artificial intelligence](https://en.wikipedia.org/wiki/Deep_learning) techniques. By allowing players to host their own scripts, gameplay can scale to complex emergent agent behaviors.")
+st.markdown("GitHub: [jetnew/open-worlds](https://github.com/jetnew/open-worlds)")
 
-fig, ax = plt.subplots()
-
+# Initialise
 response = requests.get('https://open-worlds.herokuapp.com/')
-# response = requests.get('http://127.0.0.1:8000')
 state = json.loads(response.text)
 game_state = state['state']
-ax_obj = ax.imshow(np.array(game_state))
 
 while True:
     # Query from server
     response = requests.get('https://open-worlds.herokuapp.com/')
-    # response = requests.get('http://127.0.0.1:8000')
     state = json.loads(response.text)
     game_time = state['time']
-    game_state = state['state']
-    game_scores = [f"Agent {str(i)}: {s}" for i, s in state['scores'].items()]
+    game_state = np.array(state['state'], dtype=np.uint8)
+    game_scores = state['scores']
 
-    # Display Streamlit elements
+    # Set agent scores in dataframe
+    agent_idxs, agent_scores = [], []
+    for idx, score in game_scores.items():
+        agent_idxs.append(idx)
+        agent_scores.append(score)
+    df_game_scores = pd.DataFrame({
+        "Agent": agent_idxs,
+        "Score": agent_scores,
+    })
+
+    # Refresh Streamlit elements
     st_time.text(f"Time: {game_time}")
-    st_scores.text(f"Scores: {', '.join(game_scores)}")
-    ax_obj.set_data(np.array(game_state))
-    st_state.pyplot(fig)
+    st_scores.dataframe(df_game_scores)
+
+    image = np.repeat(np.repeat(game_state, 15, axis=0), 15, axis=1)
+    image *= 25
+    image = cv2.applyColorMap(image, cv2.COLORMAP_HOT)
+    st_state.image(image)
 
     time.sleep(0.1)
-    st_ram.text("Ram used: " + str(get_ram()))
