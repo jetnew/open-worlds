07ef5acce813d8db627f7a036b58b82d7bfd6924
jetnew <notesjet@gmail.com>
2021-05-10 23:25:24 +0800

    Added utility for procedural generation of environment

---
+++ game/procgen.py
@@ -0,0 +1,45 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from opensimplex import OpenSimplex
+
+
+def generate_points(state, n):
+    """
+    Randomly set coordinates in state to 1.
+    Args:
+        state: np.array - 2D array representing game state
+        n: int - Number of points to set to 1
+
+    Returns:
+        state: np.array - 2D array representing updated game state
+    """
+    item_idx = []
+    for i in range(n):
+        item_x = random.randint(1, state.shape[0] - 1)
+        item_y = random.randint(1, state.shape[1] - 1)
+        item_idx.append(np.array([item_x, item_y]))
+    item_idx = np.array(item_idx)
+    state[item_idx[:, 1], item_idx[:, 0]] = 1
+
+
+def generate_clusters(x_dim, y_dim):
+    gen = OpenSimplex(seed=random.randint(0,1000))
+    factor = (x_dim+y_dim) / 2
+    grid = []
+    for y in range(y_dim):
+        row = []
+        for x in range(x_dim):
+            if gen.noise2d(x / factor, y / factor) < 0.5:
+                row.append(1)
+            else:
+                row.append(0)
+        grid.append(row)
+    return np.array(grid)
+
+
+if __name__ == "__main__":
+    grid = generate_clusters(30, 30)
+    plt.title("Lake Generation")
+    plt.imshow(grid)
+    plt.show()
---
+++ game/states.py
@@ -0,0 +1,73 @@
+import random
+import numpy as np
+from game.procgen import generate_points, generate_clusters
+
+
+class GameState:
+    def __init__(self, shape=(30, 30)):
+        self.x_dim, self.y_dim = shape
+        self.state = np.zeros((self.y_dim, self.x_dim))
+
+    def get_state(self):
+        return self.state
+
+    def query_state(self, x, y):
+        return self.state[y, x]
+
+    def __repr__(self):
+        return str(self.state)
+
+
+class LakeState(GameState):
+    def __init__(self, shape=(30, 30)):
+        super().__init__(shape)
+
+    def init_state(self):
+        self.state = generate_clusters(self.x_dim, self.y_dim)
+
+
+class TreeState(GameState):
+    def __init__(self, shape=(30, 30), initial=20):
+        super().__init__(shape)
+        self.initial = initial
+        self.init_state()
+
+    def init_state(self):
+        generate_points(self.state, n=self.initial)
+
+
+class FruitState(GameState):
+    def __init__(self, shape=(30, 30), initial=30, droprate=0.05):
+        super().__init__(shape)
+        self.initial = initial
+        self.droprate = droprate
+        self.init_state()
+
+    def init_state(self):
+        generate_points(self.state, n=self.initial)
+
+    def step(self):
+        if random.random() < self.droprate:
+            x, y = random.randint(0, self.x_dim), random.randint(0, self.y_dim)
+            self.state[y, x] = 1
+
+
+if __name__ == "__main__":
+    x_dim, y_dim = 10, 10
+    n_trees = 20
+    n_fruits = 30
+
+    lake_state = LakeState(shape=(x_dim, y_dim))
+    print(lake_state)
+
+    tree_state = TreeState(shape=(x_dim, y_dim), initial=n_trees)
+    print(tree_state)
+
+    fruit_state = FruitState(shape=(x_dim, y_dim), initial=n_fruits)
+    print(fruit_state)
+
+
+
+
+
+
